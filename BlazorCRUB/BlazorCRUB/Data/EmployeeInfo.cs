ee5747611a94d22c6cb5a4aad64ea305dd6ac90b
alumno.104416@ies-azarquiel.es <dragoneitor12>
2020-06-17 17:58:02 +0200

    Commit con todo

---
+++ BlazorCRUB/BlazorCRUB/Data/EmployeeInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUB.Data
+{
+    public class EmployeeInfo
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        [Required]
+        public int City { get; set; }
+
+        [Required]
+        public int Country { get; set; }
+
+        [Required]
+        public int Gender { get; set; }
+    }
+}
---
+++ BlazorCRUB/BlazorCRUB/Migrations/20200616155023_InitialCommit.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCRUB.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(nullable: false),
+                    City = table.Column<int>(nullable: false),
+                    Country = table.Column<int>(nullable: false),
+                    Gender = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
